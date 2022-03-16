f4f2c7cb047d33dadc4c14f005937084ce5696af
ariful4082 <ariful4082@gmail.com>
2022-03-16 08:34:48 +0600

    show the html file as api response

--- index.js
+++ index.js
@@ -1,52 +1,39 @@
 const express = require("express");
+const fs = require('fs')
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
-  res.send(
-    `
-       <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>NodeJS is Awesome</title>
-    <style>
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-      }
-      body {
-        font-family: Arial, Helvetica, sans-serif;
-        font-size: 16px;
-      }
-      .container {
-        width: 50%;
-        margin: 2rem auto;
-        padding: 2rem;
-        background: #ddd;
-
-      }
-    </style>
-  </head>
-  <body>
-    <div class="container">
-      <h1>Hello None js You are Awesome</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nihil
-        sequi perspiciatis illo, deleniti, distinctio eos tenetur ducimus
-        officia eveniet molestias ratione quisquam nobis, vitae dolor itaque
-        enim vel fugit.
-      </p>
-    </div>
-  </body>
-</html>
-
-       `
-  );
+ fs.readFile('./pages/index.html', (err, data)=>{
+   if(err){
+     console.log(err);
+     res.send('<h1>Something wen wrong</h1>')
+   }else{
+     res.write(data);
+     res.end()
+   }
+ })
+});
+app.get("/about", (req, res) => {
+  fs.readFile('./pages/about.html', (err, data)=>{
+    if(err){
+      res.send('<h1>Something is wrong</h1>')
+    }else{
+      res.write(data)
+      res.end()
+    }
+  })
+});
+app.get("/help", (req, res) => {
+  fs.readFile('./pages/help.html', (err, data)=>{
+    if(err){
+      res.send('<h1>Something is wrong</h1>')
+    }else{
+      res.write(data)
+      res.end()
+    }
+  })
 });
 
 app.listen(PORT, () => {
