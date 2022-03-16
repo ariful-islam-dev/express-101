609b660684efb18accd1cd4212d26cf6917add7a
ariful4082 <ariful4082@gmail.com>
2022-03-16 07:21:05 +0600

    express application setup

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello NodeJS');
+const express = require('express');
+
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is listening on PORT ${PORT}`);
+})
