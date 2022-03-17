16f137742fae42cad2b4ed0384eb7c4e728fd998
ariful4082 <ariful4082@gmail.com>
2022-03-18 00:25:44 +0600

    render dynamic data

--- controllers.js
+++ controllers.js
@@ -1,28 +1,16 @@
 const fs = require("fs");
 
 exports.homeController = (req, res) => {
-  const title = "Dynamic Title";
-  res.render("home", { title });
+  const title = "Home Page";
+  res.render("pages/home", { title });
 };
 
 exports.aboutController = (req, res) => {
-  fs.readFile("./pages/about.html", (err, data) => {
-    if (err) {
-      res.send("<h1>Something is wrong</h1>");
-    } else {
-      res.write(data);
-      res.end();
-    }
-  });
+  const title = "About Page";
+  res.render("pages/about", { title });
 };
 
 exports.helpController = (req, res) => {
-  fs.readFile("./pages/help.html", (err, data) => {
-    if (err) {
-      res.send("<h1>Something is wrong</h1>");
-    } else {
-      res.write(data);
-      res.end();
-    }
-  });
+  const title = "Help Page";
+  res.render("pages/help", { title });
 };
